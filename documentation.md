23a3d78732842d0ee4a38eee9bde24facc01ba3a
cordeirolucas42 <cordeirolucas42@hotmail.com>
2020-08-31 21:09:45 -0300

    added heroku configuration

---
+++ documentation.md
@@ -0,0 +1,6 @@
+- Some tutorials/links used in this project:
+    - https://devcenter.heroku.com/articles/preparing-a-codebase-for-heroku-deployment
+    - https://agilewarrior.wordpress.com/2014/05/16/how-to-create-a-named-app-in-heroku/
+    
+- To create a name app in heroku:
+    heroku apps:create myapp
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require('path');
 const app = express()
-const port = 8000
+// const port = 8000 //using heroku's enviromental variable
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -11,5 +11,5 @@ app.get("/",(req,res) => {
 })
 
 app.listen(port, () => {
-    console.log("Listening at port: " + port)
+    console.log("Listening at port: " + process.env.PORT)
 })
